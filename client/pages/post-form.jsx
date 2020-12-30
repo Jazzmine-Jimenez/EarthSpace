f2e0b0a015bfcb2d9609ac0ec6e5c3ac7ad22108
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2020-12-29 17:59:44 -0800

    Complete event handler to update the state when user submits and resets the form

--- client/pages/post-form.jsx
+++ client/pages/post-form.jsx
@@ -5,8 +5,10 @@ export default class PostForm extends React.Component {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { imageAdded: false };
-
+    this.state = {
+      file: '',
+      imagePreviewUrl: 'placeholder-image.png'
+    };
   }
 
   handleSubmit(event) {
@@ -20,99 +22,29 @@ export default class PostForm extends React.Component {
         event.target.reset();
       })
       .catch(err => console.error(err));
+    this.setState({
+      file: '',
+      imagePreviewUrl: 'placeholder-image.png'
+    });
+
   }
 
   handleChange(event) {
-    this.setState({ imageAdded: !this.state.imageAdded });
+    const reader = new FileReader();
+    const file = event.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    };
+    reader.readAsDataURL(file);
   }
 
   render() {
-    if (this.state.imageAdded === true) {
-      return (
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12">
-              <h3 className="heading">Share With Other Earthlings </h3>
-            </div>
-          </div>
-          <form id="post-form" onSubmit={this.handleSubmit}>
-            <div className="row form-group">
-              <div className="col-sm-12">
-                <label htmlFor="title">Title: </label>
-                <input required type="text" className="form-control" name="title" id="title" />
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="row">
-                <div className="col-sm-12">
-                  <label>Tags (Choose all that apply):</label>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-sm-4">
-                  <label className="checkbox-inline tags">
-                    <input type="checkbox" id="option1" name="tags" value="reduce" /> Reduce
-                </label>
-                </div>
-                <div className="col-sm-4">
-                  <label className="checkbox-inline tags">
-                    <input type="checkbox" id="option2" name="tags" value="recycle" /> Recycle
-                </label>
-                </div>
-                <div className="col-sm-4">
-                  <label className="checkbox-inline tags">
-                    <input type="checkbox" id="option3" name="tags" value="reuse" /> Reuse
-                </label>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-sm-4">
-                  <label className="checkbox-inline tags">
-                    <input type="checkbox" id="option4" name="tags" value="simple" /> Simple
-                </label>
-                </div>
-                <div className="col-sm-4">
-                  <label className="checkbox-inline tags">
-                    <input type="checkbox" id="option5" name="tags" value="consumers" /> Consumers
-                </label>
-                </div>
-                <div className="col-sm-4">
-                  <label className="checkbox-inline tags">
-                    <input type="checkbox" id="option6" name="tags" value="buisnesses" /> Buisnesses
-                </label>
-                </div>
-              </div>
-            </div>
-            <div className="row form-group">
-              <div className="col-sm-12">
-                <label htmlFor="content">What would you like to share?</label>
-                <textarea required className="form-control" name="content" id="content" cols="30" rows="10"></textarea>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-12">
-                <label> Upload image: </label>
-              </div>
-            </div>
-            <div className="image-container">
-              <div className="row form-control-file">
-                <div className="col-sm-6 file-container">
-                  <div className="image">
-                    <label htmlFor="image"> Image: </label>
-                    <input required onChange={this.handleChange} type="file" name="image" id="image" />
-                  </div>
-                </div>
-                <div className="col-sm-6">
-                  <img className="placeholder" src={event.target.value} alt="placeholder" />
-                </div>
-              </div>
-            </div>
-            <button type="submit" className="button btn btn-default">Post</button>
-          </form>
-        </div>
-      );
-    } else {
-      return (
+    const { imagePreviewUrl } = this.state;
+    return (
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
@@ -187,15 +119,13 @@ export default class PostForm extends React.Component {
                 </div>
               </div>
               <div className="col-sm-6">
-                <img className="placeholder" src="placeholder-image.png" alt="placeholder" />
+                  <img className="image-preview" src={imagePreviewUrl} alt="placeholder" />
               </div>
             </div>
           </div>
           <button type="submit" className="button btn btn-default">Post</button>
         </form>
       </div>
-      );
-    }
-
+    );
   }
 }

8f7e236ecf8f6d67a3fd6311472d69a0d6ba6764
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2021-01-02 18:33:24 -0800

    rendering mock-up of page

--- client/pages/view-post.jsx
+++ client/pages/view-post.jsx
@@ -14,41 +14,26 @@ export default class ViewPost extends React.Component {
               aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
               do eiusmod tempor incididunt ut labore et dolore
               magna aliqua.
-            <br/>
+              <br/>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                   eiusmod tempor incididunt ut labore et dolore magna
                   aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore
                   magna aliqua.
-            </p>
-          </div>
-              <div className="col-sm-6">
-                <div className="image-container pr-sm-5">
-                  <img className="photo border img-thumbnail rounded mb-2" src="recycle-image.png" alt="" />
+              </p>
+            </div>
+            <div className="col-sm-6">
+              <div className="image-container pr-sm-5">
+                <img className="photo border img-thumbnail rounded mb-2"
+                src="images\image-1609220205537.png" alt="" />
                   <p className="sub-title center-block">
                 <i className="fas fa-hashtag"></i>
                   tags, reduce, reuse </p>
-                </div>
               </div>
-            </div>
-            <hr/>
-              <div classNameName="row">
-                <div classNameName="col-sm-4">
-                  <p>hello</p>
-                </div>
-                <div classNameName="col-sm-4">
-                  <p>hello</p>
-                </div>
-                <div classNameName="col-sm-4">
-                  <h6><i classNameName="fas fa-user"></i> JazzyWazzy</h6>
-                </div>
-              </div>
-        </div>
-            <div className="row text-secondary">
-              <p>Comments:</p>
-              <p>Earthling1: hello what a lovely post, very specific! awesome!</p>
             </div>
           </div>
+        </div>
+      </div>
     );
   }
 }

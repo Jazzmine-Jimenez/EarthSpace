430647f5a89ea339bae014d768128b1b51abd7f6
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2021-01-02 18:56:15 -0800

    rendering users post from database in progress

--- client/pages/view-post.jsx
+++ client/pages/view-post.jsx
@@ -1,35 +1,54 @@
+import { post } from 'jquery';
 import React from 'react';
 
 export default class ViewPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/post/14/user/1')
+      .then(res => res.json())
+      .then(posts => {
+        this.setState({ post });
+      });
+  }
+
   render() {
     return (
       <div className="container">
-        <h3 className="title my-sm-4">What you&apos;ve Shared with Other Earthlings </h3>
-        <div className="post-container shadow p-3 mb-4 bg-white rounded text-secondary">
+        <h3 className="title heading my-sm-4">What you&apos;ve Shared with Other Earthlings </h3>
+        <div className="post-container shadow p-3 mb-4 bg-white rounded">
           <div className="row align-items-center">
-            <div className="col-sm-6 py-sm-5 px-sm-5">
-              <h4 className="title">Title of the Post</h4>
+            <div className="col-sm-7 py-sm-5 px-sm-5">
+              <h3 className="title mb-sm-4"> {post.title} </h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna
               aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
               do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-              <br/>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna
-                  aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore
-                  magna aliqua.
-              </p>
+              magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.</p>
             </div>
-            <div className="col-sm-6">
+            <div className="col-sm-5">
               <div className="image-container pr-sm-5">
                 <img className="photo border img-thumbnail rounded mb-2"
                 src="images\image-1609220205537.png" alt="" />
-                  <p className="sub-title center-block">
-                <i className="fas fa-hashtag"></i>
+                <p className="sub-title mx-sm-2">
+                  <i className="fas fa-hashtag mx-sm-1"></i>
                   tags, reduce, reuse </p>
               </div>
+            </div>
+          </div>
+          <hr/>
+          <div className="row">
+            <div className="col py-sm-3 px-sm-5">
+              <h6 className="float-end"><i className="fas fa-user"></i> JazzyWazzy</h6>
             </div>
           </div>
         </div>

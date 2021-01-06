a3f80efcfe28a7c6de09664d72b7cdff1e2e6d1d
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2021-01-06 11:20:57 -0800

    resyled buttons using bootstrap btn class

--- client/pages/edit-post.jsx
+++ client/pages/edit-post.jsx
@@ -81,7 +81,6 @@ export default class EditPost extends React.Component {
     })
       .then(res => {
         window.location.hash = '#users-posts';
-        res.json();
       });
   }
 
@@ -198,31 +197,29 @@ export default class EditPost extends React.Component {
             </div>
           </div>
           <div className="row">
-            <div className="col-sm-6 w-25 p-3">
-              <button type="submit" className="button my-3">Update</button>
+            <div className="col-12 my-3">
+              <button type="submit" className="btn btn-lg button me-3">
+                Update</button>
+              <button type="button" className="btn btn-lg button"
+              data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Delete </button>
             </div>
-            <div className="col-sm-6 w-25 p-3">
-              <button type="button" className="button m-3" data-bs-toggle="modal"
-              data-bs-target="#exampleModal">
-                Delete
-              </button>
-              <div className="modal fade" id="exampleModal" tabIndex="-1"
+            <div className="modal fade" id="exampleModal" tabIndex="-1"
               aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h5 className="modal-title" id="exampleModalLabel">
-                        Delete Post</h5>
-                      <button type="button" className="btn-close" data-bs-dismiss="modal"
-                      aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                      Are you sure you want to delete this post?</div>
-                    <div className="modal-footer">
-                      <button type="button" className="btn btn-secondary"
-                      data-bs-dismiss="modal">Cancel</button>
-                      <button onClick={this.handleDelete} type="button" data-bs-dismiss="modal" className="button-delete">Delete</button>
-                    </div>
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">
+                      Delete Post</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+                  </div>
+                  <div className="modal-body">
+                    Are you sure you want to delete this post?</div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary"
+                    data-bs-dismiss="modal">Cancel</button>
+                    <button onClick={this.handleDelete} type="button" data-bs-dismiss="modal" className="btn button">Delete</button>
                   </div>
                 </div>
               </div>

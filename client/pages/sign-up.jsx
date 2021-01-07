376dcd5a5fc97cffe33d981ce7b747ebc6e07088
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2021-01-06 17:18:58 -0800

    Refactor the component to fetch the data from the server.

--- client/pages/sign-up.jsx
+++ client/pages/sign-up.jsx
@@ -1,6 +1,40 @@
 import React from 'react';
 
 export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      username: '',
+      password: ''
+    };
+
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch('/api/auth/sign-up', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.state)
+    })
+      .then(res => {
+        event.target.reset();
+        window.location.hash = '#users-posts';
+      })
+      .catch(err => console.error(err));
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <div className="container">
@@ -17,35 +51,41 @@ export default class SignUp extends React.Component {
           </div>
         </div>
         <div className="sign-up-container m-4 mx-auto">
-          <div className="form shadow border border-1 rounded p-4">
-            <div className="row mb-3">
-              <div className="col-sm-6">
-                <input type="text" className="form-control form-control-lg border border-1 rounded"
-                  name="firstName" id="firstName" placeholder="First Name"/>
+          <form onSubmit={this.handleSubmit}>
+            <div className="shadow border border-1 rounded p-4">
+              <div className="row mb-3 form-group">
+                <div className="col-sm-6">
+                  <input type="text" className="form-control form-control-lg border border-1 rounded"
+                    onChange={this.handleChange} value={this.state.firstName}
+                    name="firstName" placeholder="First Name"/>
+                </div>
+                <div className="col-sm-6">
+                  <input type="text" className="form-control form-control-lg border border-1 rounded"
+                    onChange={this.handleChange} value={this.state.lastName}
+                    name="lastName" placeholder="Last Name" />
+                </div>
               </div>
-              <div className="col-sm-6">
-                <input type="text" className="form-control form-control-lg border border-1 rounded"
-                name="firstName" id="firstName" placeholder="Last Name" />
+              <div className="row mb-3 form-group">
+                <div className="col-12">
+                  <input type="text" className="form-control form-control-lg border border-1 rounded"
+                    onChange={this.handleChange} value={this.state.username}
+                    name="username" placeholder="Username"/>
+                </div>
+              </div>
+              <div className="row mb-3 form-group">
+                <div className="col-12">
+                  <input type="password" className="form-control form-control-lg border border-1 rounded"
+                    onChange={this.handleChange} value={this.state.password}
+                    name="password" placeholder="Password" />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12">
+                  <button type="submit" className="btn button btn-lg w-100"> Sign-up </button>
+                </div>
               </div>
             </div>
-            <div className="row mb-3">
-              <div className="col-12">
-                <input type="text" className="form-control form-control-lg border border-1 rounded"
-                name="username" id="username" placeholder="Username"/>
-              </div>
-            </div>
-            <div className="row mb-3">
-              <div className="col-12">
-                <input type="text" className="form-control form-control-lg border border-1 rounded"
-                  name="password" id="password" placeholder="Password" />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-12">
-                <button type="submit" className="btn button w-100"> Sign-up </button>
-              </div>
-            </div>
-          </div>
+          </form>
         </div>
       </div>
     );

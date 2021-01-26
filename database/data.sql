5c3d61e4100dc8e24662131e1c877d32fe1b3160
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2021-01-26 13:36:15 -0800

    2 users added when deployed

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,3 @@
 insert into "Users" ("username", "firstName", "lastName", "hashedPassword")
 values ('EarthSpaceLover', 'Demo', 'Account', '$argon2i$v=19$m=4096,t=3,p=1$gzXxEuPAHqoQPMNYqnMwfw$cjytM0/HBctlpt4rOqysVT+pg7zdptEB0jOkUogkJag')
+       ('AuroraTheCat', 'Aurora', 'Jimenez', '$argon2i$v=19$m=4096,t=3,p=1$86Qz+GJs/Juaze1V12DtQw$Kr1yJhNWUb7jjZQssAnyfnZAy2zJmT35DoRWt6LfdFc')
